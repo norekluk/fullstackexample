25d607be3da949fd735228e06fdb0350a2904a46
Lukáš Norek <norekluk@fit.cvut.cz>
2024-03-02 15:21:40 +0100

    feat: add REST API call to original source of exchange rate data

--- SpringBEExample/src/main/java/org/example/springbeexample/controller/SpringBeExampleController.java
+++ SpringBEExample/src/main/java/org/example/springbeexample/controller/SpringBeExampleController.java
@@ -1,8 +1,11 @@
 package org.example.springbeexample.controller;
 
+import org.example.springbeexample.data.ExchangeRate;
 import org.example.springbeexample.service.SpringBeExampleService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SpringBeExampleController {
@@ -13,13 +16,8 @@ public class SpringBeExampleController {
         this.service = service;
     }
 
-    @RequestMapping
-    public String helloWorld() {
-        return "Hello World from Spring Boot";
-    }
-
-    @RequestMapping("/goodbye")
-    public String goodbye() {
-        return "Goodbye from SpringBoot";
+    @RequestMapping("/exchange_rates")
+    public List<ExchangeRate> exchangeRates() {
+        return service.getExchangeRatesFromSource();
     }
 }
--- SpringBEExample/src/main/java/org/example/springbeexample/service/SpringBeExampleService.java
+++ SpringBEExample/src/main/java/org/example/springbeexample/service/SpringBeExampleService.java
@@ -2,17 +2,27 @@ package org.example.springbeexample.service;
 
 import org.example.springbeexample.data.ExchangeRate;
 import org.example.springbeexample.repository.SpringBeExampleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class SpringBeExampleService {
 
     private final SpringBeExampleRepository repository;
+    private final WebClient exchangeRatesWebApi;
 
-    public SpringBeExampleService(final SpringBeExampleRepository repository) {
+    @Autowired
+    public SpringBeExampleService(final SpringBeExampleRepository repository, final WebClient.Builder webClientBuilder) {
         this.repository = repository;
+        this.exchangeRatesWebApi = webClientBuilder
+                .build();
     }
 
     public List<ExchangeRate> findAll() {
@@ -27,4 +37,15 @@ public class SpringBeExampleService {
     public List<ExchangeRate> saveAll(List<ExchangeRate> exchangeRates) {
         return repository.saveAll(exchangeRates);
     }
+
+    public List<ExchangeRate> getExchangeRatesFromSource() {
+        Mono<List<ExchangeRate>> exchangeRatesResult = exchangeRatesWebApi
+                .get()
+                .uri("https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e")
+                .retrieve().bodyToMono(new ParameterizedTypeReference<List<ExchangeRate>>() {
+                }).log();
+
+        return exchangeRatesResult.block();
+    }
+
 }
